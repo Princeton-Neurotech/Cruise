fc1b91c32c5d405d35a0a180213262338cf7c043
Leila Hudson <lshudson@princeton.edu>
2022-01-03 20:55:45 -0500

    roadblock notification and pagecount

--- gui.py
+++ gui.py
@@ -2,124 +2,182 @@ import time
 import tkinter as tk
 from tkinter import *
 import numpy as np
-from pynput import keyboard
 import enchant
 from nltk.tokenize import word_tokenize, sent_tokenize
 import tkinter as tk
+from sys import exit
 
 class gui():
 
     def __init__(self):
-        self.global_charcount = 0
-        self.global_wordcount = 0
-        self.global_sentencecount = 0
-        self.time_last_change = time.time()
-        self.start_time = time.time()
-        self.output1 = 0
-        self.output2 = 0
-        self.output3 = 0
-        self.output4 = 0
-        self.inputUser = 0
-        self.decision = 0
+        self.globalCharcount = 0
+        self.globalWordcount = 0
+        self.globalSentencecount = 0
+        self.globcalPagecount = 0
+        self.timeLastChange = time.time()
+        self.startTime = time.time()
+        self.outputCharcount = 0
+        self.outputWordcount = 0
+        self.outputSentencecount = 0
+        self.outputPagecount = 0
+        self.outputStandby = ""
+        self.inputUserPrompt = 0
+        self.inputUserWordcountThreshold = 0
+        self.inputUserPagecountThreshold = 0
         self.window = 0
-        self.total_charcount = []
-        self.total_wordcount = []
-        self.total_sentencecount = []
-        self.total_standby = []
-
+        self.popupRoot = 0
+        self.totalCharcount = []
+        self.totalWordcount = []
+        self.totalSentencecount = []
+        self.totalpagecount = []
+        self.totalStandby = []
+        self.roadblock = False
+    
+    def popup(self):
+        if self.roadblock == True:
+            roadblockNotification = "You've hit a roadblock"
+            popupRoot = Tk()
+            popupButton = Button(popupRoot, text=roadblockNotification, font=("Verdana", 12), bg="yellow", command=exit)
+            popupRoot.geometry('400x50+700+500') 
+            popupButton.pack()
+        self.window.after(1000, self.popup)
+    
     def realtime(self):
-        self.output1.delete(0.0, "end")
-        self.output2.delete(0.0, "end")
-        self.output3.delete(0.0, "end")
-        self.output4.delete(0.0, "end")
-        # input from user
-        w = self.inputUser.get(0.0, "end")
-        # decision of whether you count spaces
-        sp = self.decision.get()
-        # count of chars
+        self.outputCharcount.delete(0.0, "end")
+        self.outputWordcount.delete(0.0, "end")
+        self.outputSentencecount.delete(0.0, "end")
+        self.outputPagecount.delete(0.0, "end")
+        self.outputStandby.delete(0.0, "end")
+        # prompt input
+        prompt = self.inputUserPrompt.get(0.0, "end")
+        # wordcount threshold input
+        wordcountThreshold = self.inputUserWordcountThreshold.get(0.0, "end")
+        # pagecount threshold input
+        pagecountThreshold = self.inputUserPagecountThreshold.get(0.0, "end")
         charcount = 0
         wordcount = 0
         sentencecount = 0
+        pagecount = 0
         standby_notification = ""
-        if sp == 1:
-            dictionary = enchant.Dict("en_US")
-            complete_sentences = sent_tokenize(w)
-            print(complete_sentences)
-            for sentence in complete_sentences:
-                c_wordcount = 0
-                for word in word_tokenize(sentence):
-                    if dictionary.check(word):
-                        if sentence[0] == sentence[0].upper():
-                            sentencecount = sentencecount + 1
-                            break
-            words = word_tokenize(w)
-            for word in words:
+        localRoadblock = False
+        dictionary = enchant.Dict("en_US")
+        completeSentences = sent_tokenize(prompt)
+        for sentence in completeSentences:
+            c_wordcount = 0
+            for word in word_tokenize(sentence):
                 if dictionary.check(word):
-                    wordcount = wordcount + 1
-            for k in w:
-                if k == "\n":
-                    continue
-                charcount = charcount + 1
-            if charcount != self.global_charcount or wordcount != self.global_wordcount or sentencecount != self.global_sentencecount:
-                self.time_last_change = time.time()
-                self.global_charcount = charcount
-                self.global_wordcount = wordcount
-                self.global_sentencecount = sentencecount
-            elif (round(time.time() - self.time_last_change, 2) > 10) and (self.start_time != 0):
-                standby_notification = "You've entered a standby"
+                    if sentence[0] == sentence[0].upper():
+                        sentencecount = sentencecount + 1
+                        break
+        words = word_tokenize(prompt)
+        for word in words:
+            if dictionary.check(word) and word != ".":
+                wordcount = wordcount + 1
+        if wordcount < int('0' + wordcountThreshold) or pagecount < int('0' + pagecountThreshold):
+            localRoadblock = True
+            self.roadblock = localRoadblock
+            self.popup()
+        else:
+            self.roadblock = False
+        for char in prompt:
+            if char == "\n":
+                continue
+            charcount = charcount + 1
+            if charcount % 100 == 0 and charcount != 0:
+                pagecount = pagecount + 1
+        if charcount != self.globalCharcount or wordcount != self.globalWordcount or sentencecount != self.globalSentencecount:
+            self.time_last_change = time.time()
+            self.globalCharcount = charcount
+            self.globalWordcount = wordcount
+            self.globalSentencecount = sentencecount
+        elif (round(time.time() - self.timeLastChange, 2) > 60) and (self.startTime != 0):
+            standbyNotification = "You've entered a standby"
+        if (round(time.time() - self.timeLastChange, 2) > 180) and (self.startTime != 0):
+            localRoadblock = True
+            self.roadblock = localRoadblock
+            self.popup()
+        else:
+            self.roadblock = False
 
-            print(self.start_time)
-            self.total_sentencecount.append(sentencecount)
-            self.total_wordcount.append(wordcount)
-            self.total_charcount.append(charcount)
-
-            print(round(time.time() - self.time_last_change, 2))
-            self.output1.insert(tk.INSERT, charcount)
-            self.output2.insert(tk.INSERT, wordcount)
-            self.output3.insert(tk.INSERT, sentencecount)
-            self.output4.insert(tk.INSERT, standby_notification)
-            self.window.after(5000, self.realtime)
-
+        self.totalSentencecount.append(sentencecount)
+        self.totalWordcount.append(wordcount)
+        self.totalCharcount.append(charcount)
+            
+        # print(round(time.time() - self.time_last_change, 2))
+        self.outputCharcount.insert(tk.INSERT, charcount)
+        self.outputWordcount.insert(tk.INSERT, wordcount)
+        self.outputSentencecount.insert(tk.INSERT, sentencecount)
+        self.outputPagecount.insert(tk.INSERT, pagecount)
+        self.outputStandby.insert(tk.INSERT, standby_notification)
+                
+        # repeatedly call realtime()
+        self.window.after(1000, self.realtime)
+        
     def initUI(self):
         times = []
-        # Creating interface
+        # Create interface
         self.window = tk.Tk()
-        self.window.title("Count Characters")
+        self.window.title("Roadblocks Project")
         self.window.geometry("500x600")
-        label = tk.Label(self.window, text="Input")
-        # Formatting
-        self.inputUser = tk.Text(self.window, width=450, height=10, font=("Helvetica", 16), wrap="word")
-        self.decision = tk.IntVar()
-        # Radio buttons for space counting
-        r1 = tk.Radiobutton(self.window, text="Start", value=1, variable=self.decision)
-        # Button to count
-        button1 = tk.Button(self.window, text="Begin", command=self.realtime)
-        label2 = tk.Label(self.window, text="Charcount")
-        # Output Block
-        self.output1 = tk.Text(self.window, width=20, height=2, font=("Helvetica", 16), wrap="word")
-        label3 = tk.Label(self.window, text="Wordcount")
-        self.output2 = tk.Text(self.window, width=20, height=2, font=("Helvetica", 16), wrap="word")
-        label4 = tk.Label(self.window, text="Sentence count")
-        self.output3 = tk.Text(self.window, width=20, height=2, font=("Helvetica", 16), wrap="word")
-        label5 = tk.Label(self.window, text="Standby")
-        self.output4 = tk.Text(self.window, width=20, height=2, font=("Helvetica", 16), wrap="word")
 
-        # Function calling
-        label.pack()
-        self.inputUser.pack()
-        r1.pack()
-        label2.pack()
-        self.output1.pack()
-        label3.pack()
-        self.output2.pack()
-        label4.pack()
-        self.output3.pack()
-        label5.pack()
-        self.output4.pack()
-        button1.pack()
+        #Textbox for prompt
+        promptLabel = tk.Label(self.window, text="Write prompt here")
+        self.inputUserPrompt = tk.Text(self.window, width=450, height=8, font=("Times New Roman", 12), wrap="word")
 
+        # Textbox for wordcount threshold
+        wordcountThresholdLabel = tk.Label(self.window, text="Wordcount threshold")
+        self.inputUserWordcountThreshold = tk.Text(self.window, width=10, height=1, font=("Helvetica", 12), wrap="word")
+        
+        pagecountThresholdLabel = tk.Label(self.window, text="Page count threshold")
+        self.inputUserPagecountThreshold = tk.Text(self.window, width=10, height=1, font=("Helvetica", 12), wrap="word")
+
+        begin = tk.Button(self.window, text="Begin", command=self.realtime)
+
+        charcountLabel = tk.Label(self.window, text="Charcount")
+        self.outputCharcount = tk.Text(self.window, width=20, height=1, font=("Helvetica", 12), wrap="word")
+
+        wordcountLabel = tk.Label(self.window, text="Wordcount")
+        self.outputWordcount = tk.Text(self.window, width=20, height=1, font=("Helvetica", 12), wrap="word")
+
+        sentencecountLabel = tk.Label(self.window, text="Sentence count")
+        self.outputSentencecount = tk.Text(self.window, width=20, height=1, font=("Helvetica", 12), wrap="word")
+        
+        pagecountLabel = tk.Label(self.window, text="Page count")
+        self.outputPagecount = tk.Text(self.window, width=20, height=1, font=("Helvetica", 12), wrap="word")
+
+        standbyLabel = tk.Label(self.window, text="Standby")
+        self.outputStandby = tk.Text(self.window, width=20, height=1, font=("Helvetica", 12), wrap="word")
+
+        promptLabel.pack()
+        self.inputUserPrompt.pack()
+
+        charcountLabel.pack()
+        self.outputCharcount.pack()
+
+        wordcountLabel.pack()
+        self.outputWordcount.pack()
+
+        sentencecountLabel.pack()
+        self.outputSentencecount.pack()
+
+        pagecountLabel.pack()
+        self.outputPagecount.pack()
+
+        standbyLabel.pack()
+        self.outputStandby.pack()
+
+        wordcountThresholdLabel.pack()
+        self.inputUserWordcountThreshold.pack()
+
+        pagecountThresholdLabel.pack()
+        self.inputUserPagecountThreshold.pack()
+
+        begin.pack()
+
+        self.popup()
         self.realtime()
         self.window.mainloop()
+        self.popupRoot.mainloop()
 
 def main():
     gui1 = gui()
