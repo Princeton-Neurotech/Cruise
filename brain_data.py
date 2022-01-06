4bc8c55e923fb674b2f2c285a493bb45cff8ed33
Leila Hudson <lshudson@princeton.edu>
2022-01-05 19:36:19 -0500

    brain_data, updated feature_vectors

---
+++ brain_data.py
@@ -0,0 +1,141 @@
+import argparse
+import time
+import numpy as np
+import pandas as pd
+
+import brainflow
+from brainflow.board_shim import BoardShim, BrainFlowInputParams, LogLevels
+from brainflow.data_filter import DataFilter, FilterTypes, AggOperations
+
+
+class Comms:
+
+    def __init__(self, boardID=-1, serial=''):
+        # self.data = []
+        self.isRunning = False
+        self.myBoardID = boardID
+        BoardShim.enable_dev_board_logger()
+        self.params = BrainFlowInputParams()
+        self.params.serial_port = serial
+        self.board = BoardShim(self.myBoardID, self.params)
+
+    def startStream(self):
+        """
+        Tells the bord to being streaming data
+        """
+        BoardShim.enable_dev_board_logger()
+        self.board.prepare_session()
+        # initiate stream
+        self.board.start_stream(45000, '')
+        self.isRunning = True
+        self.board.log_message(LogLevels.LEVEL_INFO, "Start sleeping in the main thread")
+        # time.sleep(sleepTime)  # sleep 30 seconds
+        # get the data
+        self.data = self.board.get_board_data()
+
+    def getData(self):
+        """
+        Gets the data from the board (presumably all of it since stream
+        was started
+        :return: The data from the board
+        """
+        return self.board.get_board_data()
+
+    def getCurrentData(self, num_samples: int):
+        """
+        Gets the current (updated) data from the board
+        :param num_samples: The amount of samples the returned ndarray will hold
+        :return: The current board data
+        """
+        return self.board.get_current_board_data(num_samples)
+
+    def get_samplingRate(self):
+        """
+        Get the rate at which the board samples data
+        (i.e.: The muse has an SR of about 256 Hz
+        :return: The sampling rate of a certain board
+        """
+        return self.board.get_sampling_rate(self.myBoardID)
+
+    def getEEGChannels(self):
+        """
+        Gets the EEG channels from the board being used
+        :return: The amt of channels for streaming EEG on the board
+        """
+        return self.board.get_eeg_channels(self.myBoardID)
+
+    def stopStream(self):
+        """
+        Tells the board to stop streaming data
+        """
+
+        if self.isRunning:
+            print('Stopping Stream')
+            self.board.stop_stream()
+            self.board.release_session()
+        else:
+            print("BOARD WAS NEVER STARTED")
+
+    def run(self, streamTime: int):
+        """
+        Tells the board to run a specific test for a specific amt of time
+        [FOR DEBUGGING]
+        :param streamTime: The time the board will be streaming
+        """
+        BoardShim.enable_dev_board_logger()
+        params = BrainFlowInputParams()
+
+        # BOARD IDs internally in brainflow
+        SYNTH_BOARD = int(-1)
+        CYTON = int(0)
+        MUSE2 = int(22)
+
+        myCytonSerialPort = 'COM3'
+        noSerial = ''
+
+        params.serial_port = noSerial
+
+        # create our board
+        # board = BoardShim(SYNTH_BOARD, params)
+        board = BoardShim(self.myBoardID, params)
+        board.prepare_session()
+
+        # initiate stream
+        board.start_stream(45000, '')
+        board.log_message(LogLevels.LEVEL_INFO, "Start sleeping in the main thread")
+        time.sleep(streamTime)  # sleep 30 seconds
+
+        # get the data
+        self.data = board.get_board_data()
+
+        # board.stop_stream()
+        # board.release_session()
+
+        print(self.data)  # for now print the data we can write it to a file
+
+    def getBoard(self):
+        """
+        Lets us know what board we are using
+        :return: The id of the board being used
+        """
+        if self.myBoardID == -1:
+            print("Default Board is being used: SYNTHETIC")
+        elif self.myBoardID == 0:
+            print("OpenBCI Cyton is being used: CYTON")
+        elif self.myBoardID == 22:
+            print("Interaxon Muse 2 is being used: MUSE2")
+        return self.myBoardID
+
+    def setBoard(self, boardID: int):
+        """
+        Change the ID of the Board we are using
+        :param boardID: -1 for Synth, 0 for Cyton, 22 for MUSE2
+        """
+        self.myBoardID = boardID
+
+
+if __name__ == "__main__":
+    myBoard = Comms(0, 'COM3')
+    myBoard.startStream()
+    time.sleep(1)
+    myBoard.stopStream()
