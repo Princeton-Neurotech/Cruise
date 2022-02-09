4c75ce49541d78f3a209a1e71aa6f193fe8bb6ec
Leila Hudson <lshudson@princeton.edu>
2022-02-09 14:38:11 -0500

    more web interface

--- chrome_extension.js
+++ chrome_extension.js
@@ -2,12 +2,9 @@ var bgl1;
 bgl1 = document.getElementsByClassName("kix-page");
 function flashRed() {
     bgl1.forEach.call(els,function(el){
-        el.style = "overflow-x: auto; background-color: rgb(80, 0, 0); height: 500px; cursor: auto; touch-action: pan-x pan-y;"
-    });
-}
+        el.style = "overflow-x: auto; background-color: rgb(80, 0, 0); height: 500px; cursor: auto; touch-action: pan-x pan-y;"});}
 function flashGrey() {
-bgl1[0].style = "overflow-x: auto; background-color: rgb(0, 0, 0); height: 500px; cursor: auto; touch-action: pan-x pan-y;"
-}
+bgl1[0].style = "overflow-x: auto; background-color: rgb(0, 0, 0); height: 500px; cursor: auto; touch-action: pan-x pan-y;"}
 
 for(let i = 0; i < 1000; i+=2) {
 setTimeout(flashRed, 40*(i))
--- web_interface.py
+++ web_interface.py
@@ -2,7 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 class selenium():
-    driver = webdriver.Firefox()
+    #options = webdriver.ChromeOptions()
+    #options.binary_location = "/Applications/Google Chrome 2.app"
+    #chrome_driver_binary = "/Users/leilahudson/Documents/GitHub/Roadblocks/chromedriver"
+    #driver = webdriver.Chrome(chrome_driver_binary, chrome_options=options)
+    driver = webdriver.Chrome(executable_path="/Users/leilahudson/Documents/GitHub/Roadblocks/chromedriver.exe")
     driver.get("http://www.python.org")
     assert "Python" in driver.title
     elem = driver.find_element_by_name("q")
