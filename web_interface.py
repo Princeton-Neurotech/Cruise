801e7f30322bbe682e8f5947b8a1d2964b7ecf3a
nick-casa <nc16@princeton.edu>
2022-02-10 02:26:53 -0500

    added JS injection in web_interface

--- web_interface.py
+++ web_interface.py
@@ -1,3 +1,4 @@
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -6,15 +7,55 @@ class selenium():
     #options.binary_location = "/Applications/Google Chrome 2.app"
     #chrome_driver_binary = "/Users/leilahudson/Documents/GitHub/Roadblocks/chromedriver"
     #driver = webdriver.Chrome(chrome_driver_binary, chrome_options=options)
-    driver = webdriver.Chrome(executable_path="/Users/leilahudson/Documents/GitHub/Roadblocks/chromedriver.exe")
-    driver.get("http://www.python.org")
-    assert "Python" in driver.title
-    elem = driver.find_element("q")
-    elem.clear()
-    elem.send_keys("pycon")
-    elem.send_keys(Keys.RETURN)
-    assert "No results found." not in driver.page_source
+    driver = webdriver.Chrome(executable_path="/Users/nickolascasalinuovo/PycharmProjects/Roadblocks/chromedriver")
+    driver.get("http://docs.google.com")
+    
+    injected_js = False
+    while(driver):
+        if driver.current_url:
+            if driver.current_url.startswith("https://docs.google.com/document/d/"):
+                # Inject JS 
+                if not injected_js:
+                    UID = driver.current_url[35:]
+                    sleep(1)
+                    driver.execute_script("var bgl1;") 
+                    driver.execute_script("parents = document.getElementsByClassName('kix-canvas-tile-content');")
+                    driver.execute_script("""
+                    window.fRed = function flashRed(){
+                        Array.from(parents).forEach(
+                            function(element,index,array){
+                                    if(element.children.length == 1){
+                                        element.children[0].style='border: 5px solid #ffcfcf;';
+                                    }
+                            });
+                        setTimeout(window.fGrey,20);
+                        return "red";
+                    }
+                    """) 
+                    driver.execute_script("""
+                    window.fGrey = function flashGrey(){
+                        Array.from(parents).forEach(
+                            function(element,index,array){
+                                if(element.children.length == 1){
+                                    element.children[0].style='border: 5px solid #ffffff;';
+                                }
+                            });
+                        setTimeout(window.fRed, 20);
+                        return "grey";
+                    }
+                    """) 
+                    driver.execute_script("window.fRed();") 
+                    injected_js = True
+
+    #assert "Python" in driver.title
+    #elem = driver.find_element("q")
+    #elem.clear()
+    #elem.send_keys("pycon")
+    #elem.send_keys(Keys.RETURN)
+    #assert "No results found." not in driver.page_source
     driver.close()
+
+    
 
 """
 from selenium import webdriver
