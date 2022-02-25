a6a2a34f1938858ca23dc83d0cc811899f53803d
nick-casa <nc16@princeton.edu>
2022-02-24 22:16:49 -0500

    combined extract text with web interface

--- extract_text.py
+++ extract_text.py
@@ -1,6 +1,3 @@
-"""
-Recursively extracts the text from a Google Doc.
-"""
 from __future__ import print_function
 from encodings import utf_8
 from time import sleep
@@ -12,106 +9,92 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import os 
-from flask import Flask
-
-import only_keyboard_features
+#import only_keyboard_features
 
 SCOPES = ['https://www.googleapis.com/auth/documents.readonly']
 DISCOVERY_DOC = 'https://docs.googleapis.com/$discovery/rest?version=v1'
-# 'https://docs.google.com/document/d/'
-DOCUMENT_ID = '1fxXMiOqWnYOWrH-t9M-U8CpDdr9OrJy3snUp-cdmdts'
 
-app = Flask(__name__)
+#DOCUMENT_ID = '1fxXMiOqWnYOWrH-t9M-U8CpDdr9OrJy3snUp-cdmdts'
 
-def get_credentials():
-    """Gets valid user credentials from storage.co
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth 2.0 flow is completed to obtain the new credentials.
+class textExtractor():
+    
+    def get_credentials(self):
+        """Gets valid user credentials from storage.co
+        If nothing has been stored, or if the stored credentials are invalid,
+        the OAuth 2.0 flow is completed to obtain the new credentials.
 
-    Returns:
-        Credentials, the obtained credential.
-    """
-
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    else:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+        Returns:
+            Credentials, the obtained credential.
+        """
+        creds = None
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-    return creds
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
+        return creds
 
-def read_paragraph_element(element):
-    """Returns the text in the given ParagraphElement.
+    def read_paragraph_element(self, element):
+        """Returns the text in the given ParagraphElement.
 
-        Args:
-            element: a ParagraphElement from a Google Doc.
-    """
-    text_run = element.get('textRun')
-    if not text_run:
-        return ''
-    return text_run.get('content')
+            Args:
+                element: a ParagraphElement from a Google Doc.
+        """
+        text_run = element.get('textRun')
+        if not text_run:
+            return ''
+        return text_run.get('content')
 
-def read_strucutural_elements(elements):
-    """Recurses through a list of Structural Elements to read a document's text where text may be
-        in nested elements.
+    def read_strucutural_elements(self, elements):
+        """Recurses through a list of Structural Elements to read a document's text where text may be
+            in nested elements.
 
-        Args:
-            elements: a list of Structural Elements.
-    """
-    text = ''
-    for value in elements:
-        if 'paragraph' in value:
-            elements = value.get('paragraph').get('elements')
-            for elem in elements:
-                text += read_paragraph_element(elem)
-        elif 'table' in value:
-            # The text in table cells are in nested Structural Elements and tables may be
-            # nested.
-            table = value.get('table')
-            for row in table.get('tableRows'):
-                cells = row.get('tableCells')
-                for cell in cells:
-                    text += read_strucutural_elements(cell.get('content'))
-        elif 'tableOfContents' in value:
-            # The text in the TOC is also in a Structural Element.
-            toc = value.get('tableOfContents')
-            text += read_strucutural_elements(toc.get('content'))
-            print(text)
-    return text
+            Args:
+                elements: a list of Structural Elements.
+        """
+        text = ''
+        for value in elements:
+            if 'paragraph' in value:
+                elements = value.get('paragraph').get('elements')
+                for elem in elements:
+                    text += self.read_paragraph_element(elem)
+            elif 'table' in value:
+                # The text in table cells are in nested Structural Elements and tables may be
+                # nested.
+                table = value.get('table')
+                for row in table.get('tableRows'):
+                    cells = row.get('tableCells')
+                    for cell in cells:
+                        text += self.read_strucutural_elements(cell.get('content'))
+            elif 'tableOfContents' in value:
+                # The text in the TOC is also in a Structural Element.
+                toc = value.get('tableOfContents')
+                text += self.read_strucutural_elements(toc.get('content'))
+        return text
 
-@app.route("/")
-def main():
-    """Uses the Docs API to print out the text of a document."""
-    credentials = get_credentials()
-    ## build(serviceName, version, http=None, discoveryServiceUrl=DISCOVERY_URI, developerKey=None, model=None, requestBuilder=HttpRequest, credentials=None, cache_discovery=True, cache=None, client_options=None, adc_cert_path=None, adc_key_path=None, num_retries=1)
-    docs_service = discovery.build('docs', 'v1', credentials=credentials, discoveryServiceUrl=DISCOVERY_DOC)
-    
-    kb = only_keyboard_features.keyboard()
-
-    for i in range(1000):
-        doc = docs_service.documents().get(documentId=DOCUMENT_ID).execute()
+    def retrieveText(self, documentID):
+        """Uses the Docs API to print out the text of a document."""
+        credentials = self.get_credentials()
+        docs_service = discovery.build('docs', 'v1', credentials=credentials, discoveryServiceUrl=DISCOVERY_DOC)
+        
+        doc = docs_service.documents().get(documentId=documentID).execute()
         doc_content = doc.get('body').get('content')
-    
+        
         # Outputs: String 
-        text = read_strucutural_elements(doc_content)
-        print("Text:" + text)
-        kb.realtime(text)
-        kb.getData()
-        sleep(5)
-    
+        text = self.read_strucutural_elements(doc_content)
+               
+        return text
+        
 
-    return text
-    
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    #    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
--- web_interface.py
+++ web_interface.py
@@ -4,54 +4,59 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+import only_keyboard_features
+import extract_text
 
 class selenium():
-    s=Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=s)
-    # driver = webdriver.Chrome(executable_path="/Users/leilahudson/Documents/GitHub/Roadblocks/chromedriver.exe")
-    driver.get("http://docs.google.com")
-    
-    injected_js = False
-    while(driver):
-        if driver.current_url:
-            if driver.current_url.startswith("https://docs.google.com/document/d/"):
-                # Inject JS 
-                if not injected_js:
-                    UID = driver.current_url[35:]
-                    sleep(1)
-                    driver.execute_script("var bgl1;") 
-                    driver.execute_script("parents = document.getElementsByClassName('kix-canvas-tile-content');")
-                    driver.execute_script("""
-                    window.fRed = function flashRed(){
-                        Array.from(parents).forEach(
-                            function(element,index,array){
+        kb = only_keyboard_features.keyboard()
+        extractor = extract_text.textExtractor()
+        
+        s=Service(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=s)
+        driver.get("https://drive.google.com/drive/u/1/my-drive")
+        
+        in_docs = False
+        while(driver):
+            if driver.current_url:
+                if driver.current_url.startswith("https://docs.google.com/document/d/"):
+                    # Inject JS 
+                    if not in_docs:
+                        UID = driver.current_url[35:-5]
+                        sleep(1)
+                        driver.execute_script("var bgl1;") 
+                        driver.execute_script("parents = document.getElementsByClassName('kix-canvas-tile-content');")
+                        driver.execute_script("""
+                        window.fRed = function flashRed(){
+                            Array.from(parents).forEach(
+                                function(element,index,array){
+                                        if(element.children.length == 1){
+                                            element.children[0].style='border: 5px solid #ffcfcf;';
+                                        }
+                                });
+                            setTimeout(window.fGrey,20);
+                            return "red";
+                        }
+                        """) 
+                        driver.execute_script("""
+                        window.fGrey = function flashGrey(){
+                            Array.from(parents).forEach(
+                                function(element,index,array){
                                     if(element.children.length == 1){
-                                        element.children[0].style='border: 5px solid #ffcfcf;';
+                                        element.children[0].style='border: 5px solid #ffffff;';
                                     }
-                            });
-                        setTimeout(window.fGrey,20);
-                        return "red";
-                    }
-                    """) 
-                    driver.execute_script("""
-                    window.fGrey = function flashGrey(){
-                        Array.from(parents).forEach(
-                            function(element,index,array){
-                                if(element.children.length == 1){
-                                    element.children[0].style='border: 5px solid #ffffff;';
-                                }
-                            });
-                        setTimeout(window.fRed, 20);
-                        return "grey";
-                    }
-                    """) 
-                    driver.execute_script("window.fRed();") 
-                    injected_js = True
+                                });
+                            setTimeout(window.fRed, 20);
+                            return "grey";
+                        }
+                        """) 
+                        driver.execute_script("window.fRed();") 
+                        in_docs = True
+                        for i in range(1000):
+                            text = extractor.retrieveText(UID)
+                            kb.realtime(text)
+                            kb.getData()
+                            sleep(5)
 
-    #assert "Python" in driver.title
-    #elem = driver.find_element("q")
-    #elem.clear()
-    #elem.send_keys("pycon")
-    #elem.send_keys(Keys.RETURN)
-    #assert "No results found." not in driver.page_source
-    driver.close()
+
+        driver.close()
+
