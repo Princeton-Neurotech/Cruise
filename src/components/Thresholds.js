7d7385d2d751f27eeaaafab4dfd3181339a6f006
Leila Hudson <lshudson@princeton.edu>
2022-10-22 20:56:28 -0400

    correct url

--- src/components/Thresholds.js
+++ src/components/Thresholds.js
@@ -73,7 +73,7 @@ class Thresholds extends React.Component {
  
     let res = true;
     if (res) {
-        axios.post("http://127.0.0.1:3001/api/thr/", { wordCount: this.state.wordCount,
+        axios.post("https://cruise-extension.herokuapp.com/api/thr/", { wordCount: this.state.wordCount,
                                                         pageCount: this.state.pageCount })
             .then(res => {
                 console.log(res);
@@ -105,11 +105,11 @@ class Thresholds extends React.Component {
   })
 
   sendML = (() => {
-    axios.get("http://127.0.0.1:3001/api/ml/")
+    axios.get("https://cruise-extension.herokuapp.com/api/ml/")
 })
 
   checkRoadblock() { 
-    axios.get("http://127.0.0.1:3001/api/roadblock") 
+    axios.get("https://cruise-extension.herokuapp.com/api/roadblock") 
       .then(res => {
               if (res.data[0] == 'True') {
                   console.log("roadblock notifs");
@@ -139,7 +139,7 @@ class Thresholds extends React.Component {
       }
 
   checkCompletion() {
-    axios.get("http://127.0.0.1:3001/api/completion") 
+    axios.get("https://cruise-extension.herokuapp.com/api/completion") 
           .then(res => {
               if (res.data[0] == 'True') {
                   this.state.notRun = true;
