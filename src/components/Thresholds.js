fcfc70ea62a5859207091bc3ab63d83fa051b0a3
Leila Hudson <lshudson@princeton.edu>
2022-11-02 10:22:04 -0400

    correct identation

--- src/components/Thresholds.js
+++ src/components/Thresholds.js
@@ -14,23 +14,6 @@ window.Swal = Swal;
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-/*
-const functions = require('firebase-functions');
-const express = require('express');
-
-const app = express();
-// middleware
-app.use(express.json({ extended: false }));
-
-// Define Routes
-app.use('/api/thr');
-app.use('/api/roadblock');
-app.use('/api/completion');
-app.use('/api/ml');
-*/
-
-var cors = require('cors');
-const port = process.env.PORT || 80;
 class Thresholds extends React.Component {
     constructor(props) {
         super(props);
@@ -44,55 +27,54 @@ class Thresholds extends React.Component {
         };
     }
 
-  showRoadblockNotifications() {
-      console.log(this.n.supported())
-      if (this.n.supported()); 
-      this.n.show();
-  }
+    showRoadblockNotifications() {
+        console.log(this.n.supported())
+        if (this.n.supported()); 
+        this.n.show();
+    }
 
-  showCompNotifications() {
-      console.log(this.a.supported())
-      if (this.a.supported());
-      this.a.show();
-  }
+    showCompNotifications() {
+        console.log(this.a.supported())
+        if (this.a.supported());
+        this.a.show();
+    }
 
-  handleRoadClick(event) {
-      this.n.close(event.target.tag);
-  }
-  handleCompClick(event) {
-      this.a.close(event.target.tag);
-  }
+    handleRoadClick(event) {
+        this.n.close(event.target.tag);
+    }
+    handleCompClick(event) {
+        this.a.close(event.target.tag);
+    }
 
-  checkWildcard = (() => {
-    axios.post("https://cruise-extension.herokuapp.com:80/api/wildcard", "wildcard").then(res => {
-        console.log("post")                                                    
-        console.log(res);
-        if (!res.ok) {
-            Swal.fire(
-                'Unsuccessful',
-                'Unable to delete selected user. Please contact administrator.',
-                'error'
-            );
-            return;
-        }
-        Swal.fire({
-            title: 'Wildcard!',
-            text: "",
-            icon: 'OK',
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'OK',
+    checkWildcard = (() => {
+        axios.post("https://cruise-extension.herokuapp.com:80/api/wildcard", "wildcard").then(res => {
+            console.log("post")                                                    
+            console.log(res);
+            if (!res.ok) {
+                Swal.fire(
+                    'Unsuccessful',
+                    'Unable to delete selected user. Please contact administrator.',
+                    'error'
+                );
+                return;
+            }
+            Swal.fire({
+                title: 'Wildcard!',
+                text: "",
+                icon: 'OK',
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'OK',
+            })
         })
     })
-  })
 
-  sendT = (() => {
-    setInterval(() => {
-        this.checkWildcard()
-    }, 5000);
-  })
-  // sendT()
-  
+    sendT = (() => {
+        setInterval(() => {
+            this.checkWildcard()
+        }, 5000);
+    })
+    // sendT()
 
 sendThr = (() => {
     // 5s for testing, final is every min
@@ -144,7 +126,7 @@ sendThr = (() => {
 //     })
 // })
 
-let res = true;
+    let res = true;
     if (res) {
         axios.post("https://cruise-extension.herokuapp.com:80/api/thr/", { wordCount: this.state.wordCount,
                                                         pageCount: this.state.pageCount }) .then(res => {
@@ -169,121 +151,115 @@ let res = true;
         })
     }
 
-sendML = (() => {
- axios.get("https://cruise-extension.herokuapp.com/api/ml", {port:80})
-})
+    sendML = (() => {
+    axios.get("https://cruise-extension.herokuapp.com/api/ml", {port:80})
+    })
 
-  checkRoadblock() { 
-    axios.get("https://cruise-extension.herokuapp.com/api/roadblock", {port:80}) 
-      .then(res => {
-              if (res.data[0] == 'True') {
-                  console.log("roadblock notifs");
-                  this.state.isRoadblock = true;
-                  if (this.state.isRoadblock == true) {
-                      Swal.fire({
-                          title: 'You have approached a roadblock!',
-                          text: "",
-                          icon: 'OK',
-                          confirmButtonColor: '#3085d6',
-                          cancelButtonColor: '#d33',
-                          confirmButtonText: 'OK',
-                          iconHtml: '<img src="/src/components/sad_whale.png">'
-                      })
-                      }   
-                      this.state.isRoadblock = false;
-                  }
-              // need to then set back to false!
-              // console.log(res);
-              // console.log(res.data);
-          }).catch (
-              function (error) {
-                  console.log('Show error notification! ' + error)
-                  return Promise.reject(error)
-               }
-          )
-      }
+    checkRoadblock() = () => { 
+        axios.get("https://cruise-extension.herokuapp.com/api/roadblock", {port:80}).then(res => {
+            if (res.data[0] == 'True') {
+                console.log("roadblock notifs");
+                this.state.isRoadblock = true;
+                if (this.state.isRoadblock == true) {
+                    Swal.fire({
+                        title: 'You have approached a roadblock!',
+                        text: "",
+                        icon: 'OK',
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'OK',
+                        iconHtml: '<img src="/src/components/sad_whale.png">'
+                    })
+                }   
+                this.state.isRoadblock = false;
+            }
+            // need to then set back to false!
+            // console.log(res);
+            // console.log(res.data);
+            }).catch(
+            function (error) {
+                console.log('Show error notification! ' + error)
+                return Promise.reject(error)
+            }
+        )
+    }
 
-  checkCompletion() {
-    axios.get("https://cruise-extension.herokuapp.com/api/completion", {port:80}) 
-          .then(res => {
-              if (res.data[0] == 'True') {
-                  this.state.notRun = true;
-                  console.log("completion notifs");
-                  // this.showCompNotifications();
-                  this.state.isCompletion = true;
-                  if (this.state.isCompletion == true) {
-                  Swal.fire({
-                      title: 'You have completed your goal!',
-                      text: "",
-                      icon: 'OK',
-                      confirmButtonColor: '#3085d6',
-                      cancelButtonColor: '#d33',
-                      confirmButtonText: 'OK',
-                      iconHtml: '<img src="/src/components/happy_whale.png">'
-                  })
-              }
-              this.state.isCompletion = false;
-              }
-              // console.log(res);
-              // console.log(res.data);
-          }).catch (
-              function (error) {
-                  console.log('Show error notification! ' + error)
-                  return Promise.reject(error)
-              }
-          )
-  }
+    checkCompletion = () => {
+        axios.get("https://cruise-extension.herokuapp.com/api/completion", {port:80}).then(res => {
+            if (res.data[0] == 'True') {
+                this.state.notRun = true;
+                console.log("completion notifs");
+                // this.showCompNotifications();
+                this.state.isCompletion = true;
+                if (this.state.isCompletion == true) {
+                Swal.fire({
+                    title: 'You have completed your goal!',
+                    text: "",
+                    icon: 'OK',
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'OK',
+                    iconHtml: '<img src="/src/components/happy_whale.png">'
+                })
+            }
+            this.state.isCompletion = false;
+            }
+            // console.log(res);
+            // console.log(res.data);
+            }).catch (
+            function (error) {
+            console.log('Show error notification! ' + error)
+            return Promise.reject(error)
+        })
+    }
 
-  handleCallbackOne = (childData) =>{
-      // console.log(childData);
-      // this.setState({LineSpace: childData})
-      this.setState({ ...this.state , wordCount: childData });
-  }
+    handleCallbackOne = (childData) =>{
+        // console.log(childData);
+        // this.setState({LineSpace: childData})
+        this.setState({ ...this.state , wordCount: childData });
+    }
 
-  handleCallbackTwo = (childData) =>{
-      console.log(childData);
-      // this.setState({LineSpace: childData})
-      
-      this.setState({ ...this.state , pageCount: childData });
-  }
+    handleCallbackTwo = (childData) =>{
+        console.log(childData);
+        // this.setState({LineSpace: childData})
+        this.setState({ ...this.state , pageCount: childData });
+    }
 
-  handleButtonClick() {
-      if(this.state.ignore) {
-        return;
-      }
-  
-      const now = Date.now();
-  
-      const title = 'React-Web-Notification' + now;
-      const body = 'Hello' + new Date();
-      const tag = now;
-      // const icon = 'http://mobilusoss.github.io/react-web-notification/example/Notifications_button_24.png';
-      // const icon = 'http://localhost:3000/Notifications_button_24.png';
-  
-      // Available options
-      // See https://developer.mozilla.org/en-US/docs/Web/API/Notification/Notification
-      const options = {
+    handleButtonClick() = () => {
+        if(this.state.ignore) {
+            return;
+        }
+        const now = Date.now();
+        const title = 'React-Web-Notification' + now;
+        const body = 'Hello' + new Date();
+        const tag = now;
+        // const icon = 'http://mobilusoss.github.io/react-web-notification/example/Notifications_button_24.png';
+        // const icon = 'http://localhost:3000/Notifications_button_24.png';
+    
+        // Available options
+        // See https://developer.mozilla.org/en-US/docs/Web/API/Notification/Notification
+        const options = {
         tag: tag,
         body: body,
         // icon: icon,
         lang: 'en',
         dir: 'ltr',
         // sound: './sound.mp3'  // no browsers supported https://developer.mozilla.org/en/docs/Web/API/notification/sound#Browser_compatibility
-      }
-      this.setState({
+        }
+        this.setState({
         title: title,
         options: options
-      });
+        });
     }
   
-    handleButtonClick2() {
+    handleButtonClick2() = () => {
       this.props.swRegistration.getNotifications({}).then(function(notifications) {
         console.log(notifications);
       });
     }
     
-  // const [open, setOpen] = useState(false);
-  render() {
+    // const [open, setOpen] = useState(false);
+    render() = () => {
       return (
           <div className='thresholdsList'>
               <ReactNotifications
@@ -323,7 +299,8 @@ sendML = (() => {
                   <button className="btn btn-success" onClick={this.sendThr}>submit</button>
               </div>
           </div>)
-      }
+        }
+    })
 }
 
 export default Thresholds
